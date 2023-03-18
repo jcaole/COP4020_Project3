f8b14ea295390ce0bb8eb600bb64bdfbdaacee79
jcaole <jeremy_1072@yahoo.com>
2023-03-18 02:15:11 -0500

    working batch file

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Running program with legal1.txt...
-python fsa.py fsa.txt legal1.txt
+py fsa.py fsa.txt legal1.txt
 
 echo.
 echo Running program with illegal1.txt...
-python fsa.py fsa.txt illegal1.txt
+py fsa.py fsa.txt illegal1.txt
